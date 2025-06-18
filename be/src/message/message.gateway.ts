88cc5db8467be14a0fecdcd295c01b249695de25
FordangIT <fordang0819@gmail.com>
2025-06-18 18:01:29 +0900

    Feat: 웹소켓 기본 기능

--- be/src/message/message.gateway.ts
+++ be/src/message/message.gateway.ts
@@ -1,9 +1,42 @@
-import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import {
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+  MessageBody,
+} from '@nestjs/websockets';
+import { Server } from 'socket.io';
+import MessageService from './message.service';
 
-@WebSocketGateway()
+@WebSocketGateway({
+  cors: {
+    origin: '*',
+  },
+})
 export class MessageGateway {
-  @SubscribeMessage('message')
-  handleMessage(client: any, payload: any): string {
-    return 'Hello world!';
+  @WebSocketServer()
+  server: Server;
+
+  constructor(private readonly messageService: MessageService) {}
+
+  @SubscribeMessage('sendMessage')
+  async handleMessage(
+    @MessageBody() data: { roomId: string; content: string; sender: string },
+  ) {
+    const savedMessage = await this.messageService.create(data);
+
+    //클라이언트에 전파
+    this.server.to(data.roomId).emit('message', {
+      id: savedMessage._id,
+      sender: savedMessage.sener,
+      content: savedMessage.content,
+      timestamp: savedMessage.timestamp,
+    });
+  }
+
+  handleConnection(client: any) {
+    const roomId = client.handshake.query.roomId;
+    if (roomId) {
+      client.join(roomId);
+    }
   }
 }
--- be/src/message/message.module.ts
+++ be/src/message/message.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { MessageModule } from '@nestjs/mongoose';
+import { Message, MessageSchema } from './message.schema';
+import { MessageService } from './message.service';
 import { MessageController } from './message.controller';
 import { MessageGateway } from './message.gateway';
+import { Mongoose } from 'mongoose';
 
 @Module({
+  imports: [
+    Mongoose.forFeature([{ name: Message.name, schema: MessageSchema }]),
+  ],
+  providers: [MessageGateway],
   controllers: [MessageController],
-  providers: [MessageGateway]
 })
 export class MessageModule {}
